102c61b7699770d5c422398a9699e219b181753d
Martin Dörfelt <martin.d@andix.de>
2021-10-10 15:02:02 +0200

    fix type

--- joppy/tools.py
+++ joppy/tools.py
@@ -3,7 +3,7 @@
 import base64
 
 
-def encode_base64(filepath: str) -> bytes:
+def encode_base64(filepath: str) -> str:
     """Encode an arbitrary file to base64."""
     with open(filepath, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode("utf-8")
