85740e8befd87e11cc99032b129491e3620cc1c3
Martin Dörfelt <martin.d@andix.de>
2021-10-29 14:00:31 +0200

    increment version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="joppy",
-    version="0.0.2",
+    version="0.0.3",
     packages=["joppy"],
     description="Python API for Joplin",
     long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
