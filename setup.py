ccac2c1d2d137f0f8948c216270894e944a2eccd
Martin Dörfelt <martin.d@andix.de>
2023-02-25 10:36:06 +0100

    increment version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="joppy",
-    version="0.1.0",
+    version="0.1.1",
     packages=["joppy"],
     # https://stackoverflow.com/a/70386281/7410886
     package_data={
