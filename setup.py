17d880ab3597881e458740eb1758d3ef6b5f0955
Martin Dörfelt <martin.d@andix.de>
2023-02-25 10:51:57 +0100

    add some pypi classifiers

--- setup.py
+++ setup.py
@@ -17,14 +17,17 @@ setup(
     author="Martin Dörfelt",
     author_email="martin.d@andix.de",
     license="Mozilla Public License version 2.0",
+    # https://pypi.org/classifiers
     classifiers=[
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Typing :: Typed",
     ],
     python_requires=">=3.6",
     install_requires=["requests"],
