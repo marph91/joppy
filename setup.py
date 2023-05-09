55ae63a409c6485bd1c08323392e78371c7105aa
Martin Dörfelt <martin.d@andix.de>
2023-05-09 10:47:26 +0200

    test python 3.11

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Typing :: Typed",
     ],
     python_requires=">=3.6",
