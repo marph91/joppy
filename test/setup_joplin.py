2e2d303c3ccbdd2f1de152be3a6dc74d8e5a1def
Martin Dörfelt <martin.d@andix.de>
2023-05-09 10:34:16 +0200

    update to latest joplin release 2.10.17

--- test/setup_joplin.py
+++ test/setup_joplin.py
@@ -20,7 +20,7 @@ def download_joplin(destination: str) -> None:
         #    "https://api.github.com/repos/laurent22/joplin/releases"
         # )
         # latest_version = response.json()[0]["name"].lstrip("v")
-        latest_version = "2.9.17"
+        latest_version = "2.10.17"
         print(f"Testing with Joplin version {latest_version}.")
 
         # download the binary
