ddfff6312013b63e38a8de45e373779a08765224
Martin Dörfelt <martin.d@andix.de>
2025-04-04 18:48:21 +0200

    remove deprecated classifier

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
     license="Mozilla Public License version 2.0",
     # https://pypi.org/classifiers
     classifiers=[
-        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
